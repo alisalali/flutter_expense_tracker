af7dbad702ff826436c1eabb8bfb529d9f4bb89e
alisalali <alis.alali@hotmail.com>
2023-09-08 04:43:44 +0300

    define custom type predefine value models

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,15 +2,24 @@ import 'package:uuid/uuid.dart'; // generate unique id , install this using $ fl
 
 const uuid = Uuid(); //initialize class for uuid package
 
+enum Category {
+  food,
+  travel,
+  leisure,
+  work,
+} //define custom type predefine value
+
 class Expense {
   Expense({
     required this.title,
     required this.amount,
     required this.date,
+    required this.category,
   }) : id = uuid.v4(); //assign random id string with uuid package
 
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 }
