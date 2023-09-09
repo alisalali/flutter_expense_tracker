0c83cfa491fc7f314c0af40a77f312d5bcf8938e
alisalali <alis.alali@hotmail.com>
2023-09-10 02:19:59 +0300

    getting keystroke inputs value

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -11,21 +11,45 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  var _enteredTitle = ''; // global var for title var
+
+  void _onSaveTitle(String inputValue) {
+    /* 
+    This function handle key stroke input value then
+    store it in  _enteredTitle var
+    
+     */
+    _enteredTitle = inputValue; // store input stroke to title var
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           // Creates a Material Design text field.
           TextField(
+            onChanged:
+                _onSaveTitle, // will pass keystroke to _onSaveTitle function
             maxLength: 50,
             keyboardType: TextInputType.name,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               //The decoration to show around the text field.
               label: Text("Title"),
             ),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  // execute event to save inputs field
+                  print(_enteredTitle);
+                },
+                child: const Text("Save Expense"),
+              )
+            ],
+          )
         ],
       ),
     );
