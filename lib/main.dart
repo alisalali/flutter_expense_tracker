9443f7fcc3d30981ae08e366fb622d57cd31f7cb
alisalali <alis.alali@hotmail.com>
2023-09-13 02:48:09 +0300

    getting start with theme and copy default value

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,10 @@ import 'package:expense_tracker/expenses.dart';
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true), // active material3 theme
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 240, 192, 255),
+      ), // to copy default material3 default settings
       home: const Expenses(),
     ),
   );
