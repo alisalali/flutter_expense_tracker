32d22009aed096311d9f9d80cf0d7b3403a79b06
alisalali <alis.alali@hotmail.com>
2023-09-18 02:32:53 +0300

    comments lock device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart'; //require to lock orientation
+// import 'package:flutter/services.dart'; //require to lock orientation
 
 import 'package:expense_tracker/expenses.dart';
 
@@ -14,63 +14,65 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding
-      .ensureInitialized(); // to make sure initialized widgets then set
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
-      .then((fn) => runApp(
-            MaterialApp(
-              // copy Dark theme useMaterial3 settings and modify it
-              darkTheme: ThemeData.dark().copyWith(
-                useMaterial3: true,
-                colorScheme: kDarkColorScheme,
-                cardTheme: const CardTheme().copyWith(
-                  color: kDarkColorScheme.secondaryContainer,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 8,
-                  ),
-                ),
-                elevatedButtonTheme: ElevatedButtonThemeData(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: kDarkColorScheme.primaryContainer,
-                    foregroundColor: kDarkColorScheme.onPrimaryContainer,
-                  ),
-                ),
+  // WidgetsFlutterBinding
+  //     .ensureInitialized(); // to make sure initialized widgets then set
+  // SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+  //     .then((fn) =>
+  runApp(
+    MaterialApp(
+      // copy Dark theme useMaterial3 settings and modify it
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkColorScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      // to copy default material3 default settings
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        //apply colorScheme entire theme
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        // Modify Card Theme color
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 8,
+          ),
+        ),
+        //Modify elevatedButtonTheme
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        // Modify TextsTheme widgets , with title
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondary,
+                fontSize: 16,
               ),
-              // to copy default material3 default settings
-              theme: ThemeData().copyWith(
-                useMaterial3: true,
-                //apply colorScheme entire theme
-                colorScheme: kColorScheme,
-                appBarTheme: const AppBarTheme().copyWith(
-                  backgroundColor: kColorScheme.onPrimaryContainer,
-                  foregroundColor: kColorScheme.primaryContainer,
-                ),
-                // Modify Card Theme color
-                cardTheme: const CardTheme().copyWith(
-                  color: kColorScheme.secondaryContainer,
-                  margin: const EdgeInsets.symmetric(
-                    horizontal: 12,
-                    vertical: 8,
-                  ),
-                ),
-                //Modify elevatedButtonTheme
-                elevatedButtonTheme: ElevatedButtonThemeData(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: kColorScheme.primaryContainer,
-                  ),
-                ),
-                // Modify TextsTheme widgets , with title
-                textTheme: ThemeData().textTheme.copyWith(
-                      titleLarge: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: kColorScheme.onSecondary,
-                        fontSize: 16,
-                      ),
-                    ),
-              ),
-              themeMode: ThemeMode.system, // define default color mode
-              home: const Expenses(),
             ),
-          ));
+      ),
+      themeMode: ThemeMode.system, // define default color mode
+      home: const Expenses(),
+    ),
+    // )
+  );
 }
